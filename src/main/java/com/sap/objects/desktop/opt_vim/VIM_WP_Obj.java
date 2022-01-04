ba95f6b82e083abe2cebc13b67e05de845eaf987
Pavel Popov <ppopov@worldbankgroup.org>
2022-01-04 15:12:35 +0200

    Update VIM_WP_Obj.java

--- src/main/java/com/sap/objects/desktop/opt_vim/VIM_WP_Obj.java
+++ src/main/java/com/sap/objects/desktop/opt_vim/VIM_WP_Obj.java
@@ -118,8 +118,9 @@ public class VIM_WP_Obj extends GeneralTestConfig {
         // Add text
         Obj = new ActiveXComponent(Session.invoke("findById", "wnd[0]/usr/tabsTAB_MAIN/tabpTAB1/ssubTAB_MAIN_SUBSCREEN:/OPT/SAPLVIM_PMC_UI_COMP:1001/subSELECT_SUBSCRN_MIDDLE:/OPT/SAPLVIM_PMC_UI_COMP:1200/ctxtH1_DOID-LOW").toDispatch());
         Obj.invoke("setFocus");
-        autoItX.sleep(500);
-        Obj.setProperty("text", TEST_DATA.getRequestID());
+        autoItX.sleep(1000);
+        String id = TEST_DATA.getRequestID();
+        Obj.setProperty("text", id);
         autoItX.sleep(500);
 
         // Press apply
